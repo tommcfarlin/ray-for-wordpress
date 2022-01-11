74f894621ab296a3acfaff96414a98d38b2e2d3c
Tom McFarlin <tom@tommcfarlin.com>
2022-01-11 09:44:49 -0500

    Add boilerplate code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ray for WordPress
+
+An example project for how to use Spatie Ray in WordPress Development.
---
+++ ray-for-wordpress.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Ray For WordPress
+ *
+ * An example project for how to use Spatie Ray in WordPress Development.
+ *
+ * PHP version 7.4.27
+ *
+ * @category WordPress_Plugin
+ * @package  RayForWP
+ * @author   Tom McFarlin <tom@tommcfarlin.com>
+ * @license  GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ * @link     https://github.com/tommcfarlin/wp-easy-email-export/
+ * @since    11 January 2022
+ *
+ * @wordpress-plugin
+ * Plugin Name: Ray For WordPress
+ * Plugin URI:  https://github.com/tommcfarlin/ray-for-wordpress/
+ * Description: An example project for how to use Spatie Ray in WordPress Development.
+ * Author:      Tom McFarlin <tom@tommcfarlin.com>
+ * Version:     1.0.0
+ */
+
+namespace RayForWP;
+
+defined('WPINC') || die;
+require_once __DIR__ . '/vendor/autoload.php';
