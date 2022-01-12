4b67f48d747132b9716fec6face5c70934e85c49
Tom McFarlin <tom@tommcfarlin.com>
2022-01-12 10:55:23 -0500

    Add example calls to Ray for demo purposes.

--- ray-for-wordpress.php
+++ ray-for-wordpress.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Ray For WordPress
  *
@@ -25,3 +26,37 @@ namespace RayForWP;
 
 defined('WPINC') || die;
 require_once __DIR__ . '/vendor/autoload.php';
+
+add_filter(
+    'the_content',
+    /**
+     * Renders the content to the browser from the database.
+     *
+     * @param string $content The content coming from WordPress.
+     *
+     * @return string $content The processed content to be sent to the browser.
+     */
+    function (string $content): string {
+        if (!is_single()) {
+            return $content;
+        }
+
+        $user = wp_get_current_user();
+        ray($user);
+
+        ray(
+            get_user_meta($user->data->ID, 'show_admin_bar_front', true)
+        );
+
+        $capabilities = [];
+        foreach ($user->allcaps as $key => $value) {
+            $capabilities[$key] = $value;
+            if (15 < count($capabilities)) {
+                break;
+            }
+        }
+        ray()->table($capabilities);
+
+        return $content;
+    }
+);
